bce2099fc5f53fd59b6b76e44b94d42012272da8
pe12334 <juliocesarpenaquispe8@gmail.com>
2025-04-08 13:53:26 -0500

    primer commit

---
+++ src/main/java/com/mycompany/facturacion/Facturacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.facturacion;
+
+/**
+ *
+ * @author ALUMNO
+ */
+public class Facturacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}   
